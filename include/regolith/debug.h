00b289b93c61831f4105fa6a1d14d7c39afe24df
adamuremek <adamuremek@gmail.com>
2024-05-09 14:51:46 -0400

    Modified logging

--- include/regolith/debug.h
+++ include/regolith/debug.h
@@ -1,11 +1,27 @@
 #ifndef REGOLITH_DEBUG_H
 #define REGOLITH_DEBUG_H
 
-class REGOLITH_API rDebug{
+#include <mutex>
+
+class REGOLITH_API rDebug {
 public:
-    static void doNothing(const char*){}
-    static inline std::function<void(const char*)> log = doNothing;
-    static inline std::function<void(const char*)> err = doNothing;
+    // Mutex for thread-safe logging
+    static inline std::mutex log_mutex;
+
+    // Default logging functions
+    static void defaultLog(const char* message) {
+        std::lock_guard<std::mutex> lock(log_mutex);
+        std::cout << "LOG: " << message << std::endl;
+    }
+
+    static void defaultErr(const char* message) {
+        std::lock_guard<std::mutex> lock(log_mutex);
+        std::cerr << "ERROR: " << message << std::endl;
+    }
+
+    // Function pointers for logging
+    static inline std::function<void(const char*)> log = defaultLog;
+    static inline std::function<void(const char*)> err = defaultErr;
 };
 
 #endif //REGOLITH_DEBUG_H
